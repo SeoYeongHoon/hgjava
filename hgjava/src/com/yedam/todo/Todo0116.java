296aadfecf39624d30be46b893fa13ac7d6a662b
admin <admin@DESKTOP-4P9DMP0>
2024-01-17 08:56:00 +0900

    2024 01 16 work

--- hgjava/src/com/yedam/todo/Todo0116.java
+++ hgjava/src/com/yedam/todo/Todo0116.java
@@ -6,14 +6,22 @@ public class Todo0116 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		
-		System.out.println("값을 입력 >>> ");
-		String input = sc.nextLine();
+		// while문 사용
+		while(true) {
+			System.out.println("값을 입력 >>> ");
+			String input = sc.nextLine();
+			System.out.println("입력값은 " + input + "입니다.");
+			if (input.equals("quit")) {
+				System.out.println("<< end of program >>");
+				break;
+			}
+		} 
+		// 입력값은 홍길동 입니다.
 		// input.equals("quit");
 		
-		// 입력값은 홍길동 입니다.
-		// 
-		// quit 값이 들어오면 << end of program >> 출력 : while문 사용
+		// quit 값이 들어오면 << end of program >> 출력
 		
 	}
 
